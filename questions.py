be1914ba7e2a85579a26faf182c4fde67d6f9c96
PurpleFlower436 <bhargvih12@gmail.com>
2026-05-23 20:10:32 -0500

    Moved trivia questions to questions.py

---
+++ questions.py
@@ -0,0 +1,423 @@
+MARVEL_EASY = [
+    {
+        "question": "How many infinity stones are there?",
+        "choices": {
+            "a": "ten",
+            "b": "six",
+            "c": "five",
+            "d": "two"
+        },
+        "answer": "b"
+    },
+    {
+        "question": "In which Avengers movie do the Avengers first assemble?",
+        "choices": {
+            "e": "Avengers Infinity War",
+            "f": "Avengers Age of Ultron",
+            "g": "The Avengers",
+            "h": "Avengers Endgame"
+        },
+        "answer": "g"
+    },
+    {
+        "question": "What gift does Peter give MJ in Spider-Man Far From Home?",
+        "choices": {
+            "i": "notebook",
+            "j": "black dahlia bracelet",
+            "k": "black dahlia necklace",
+            "l": "drone"
+        },
+        "answer": "k"
+    },
+    {
+        "question": "In Thor: Ragnarok, how long does Loki say he was falling for?",
+        "choices": {
+            "m": "thirty minutes",
+            "n": "twenty minutes",
+            "o": "forty minutes",
+            "p": "ten minutes"
+        },
+        "answer": "m"
+    },
+    {
+        "question": "What is the name of Thor's hammer?",
+        "choices": {
+            "q": "Peggy",
+            "r": "Jonathan",
+            "s": "Storm Breaker",
+            "t": "Mjolnir"
+        },
+        "answer": "t"
+    }
+]
+
+MARVEL_HARD = [
+    {
+        "question": "Who said this line?: “I don’t know if you’ve been in a fight before, but there's usually not this much talking",
+        "choices": {
+            "a": "Peter Parker/Spider-Man",
+            "b": "Bucky Barnes/The Winter Soldier",
+            "c": "Steve Rogers/Captain America",
+            "d": "Sam Wilson/The Falcon"
+        },
+        "answer": "d"
+    },
+    {
+        "question": "What is Morgan's first line in Avengers Endgame?",
+        "choices": {
+            "e": "Let’s go play",
+            "f": "Define lunch or be disintegrated",
+            "g": "I am Iron Man",
+            "h": "Hi Daddy"
+        },
+        "answer": "f"
+    },
+    {
+        "question": "In Spider-Man No Way Home, how does Peter trap Dr. Strange in the mirror dimension?",
+        "choices": {
+            "i": "Peter webs Dr. Strange to a piece of the canyon",
+            "j": "Peter uses his webs to create a archimedes spiral",
+            "k": "Peter webs Dr. Strange to the train",
+            "l": "Peter casts a spell that makes Dr. Strange fall through a portal"
+        },
+        "answer": "j"
+    },
+    {
+        "question": "Who said this line to Loki in the Disney+ series Loki: “Your job is to help others become the best versions of themselves”",
+        "choices": {
+            "m": "Mobius",
+            "n": "Ravonna Renslayer",
+            "o": "Sylvie",
+            "p": "Hunter B-15"
+        },
+        "answer": "m"
+    },
+    {
+        "question": "What does Tony tell Peter when he sees him being beamed up into Ebony Maw’s Q-ship?",
+        "choices": {
+            "q": "You're too high up, you're running out of air",
+            "r": "But you said save the wizard",
+            "s": "Pete, you gotta let go I’m gonna catch you",
+            "t": "Kid, where did you come from?"
+        },
+        "answer": "s"
+    }
+]
+
+HISTORY_EASY = [
+    {
+        "question": "How many world wonders are there?",
+        "choices": {
+            "a": "4",
+            "b": "7",
+            "c": "2",
+            "d": "1"
+        },
+        "answer": "b"
+    },
+    {
+        "question": "What is the tallest building in the world?",
+        "choices": {
+            "e": "Willis Tower in Chicago",
+            "f": "Burj Khalifa in Dubai",
+            "g": "Shanghai Tower in China",
+            "h": "One World Trade Center in New York City"
+        },
+        "answer": "f"
+    },
+    {
+        "question": "When did World War I start?",
+        "choices": {
+            "i": "1928",
+            "j": "1901",
+            "k": "1916",
+            "l": "1914"
+        },
+        "answer": "l"
+    },
+    {
+        "question": "Which country had the world’s first newspaper?",
+        "choices": {
+            "m": "England",
+            "n": "United States of America",
+            "o": "China",
+            "p": "Canada"
+        },
+        "answer": "o"
+    },
+    {
+        "question": "Which is the largest country in the world based on population?",
+        "choices": {
+            "q": "India",
+            "r": "United States of America",
+            "s": "China",
+            "t": "Indonesia"
+        },
+        "answer": "s"
+    }
+]
+
+HISTORY_HARD = [
+    {
+        "question": "When was the United States Declaration of Independence signed?",
+        "choices": {
+            "a": "October 15, 1776",
+            "b": "June 19, 1775",
+            "c": "August 2, 1776",
+            "d": "April 9, 1777"
+        },
+        "answer": "c"
+    },
+    {
+        "question": "What is considered the largest empire in history?",
+        "choices": {
+            "e": "Han Dynasty",
+            "f": "Mongol Empire",
+            "g": "Ottoman Empire",
+            "h": "Russian Empire"
+        },
+        "answer": "f"
+    },
+    {
+        "question": "What was the name of the group formed to maintain world peace after WWI?",
+        "choices": {
+            "i": "League of Legends",
+            "j": "League of World Peace",
+            "k": "League of Nations",
+            "l": "League of Alliance"
+        },
+        "answer": "k"
+    },
+    {
+        "question": "The Great Pyramid was built as a tomb for which pharaoh?",
+        "choices": {
+            "m": "Khufu",
+            "n": "Neferirkare Kakai",
+            "o": "Userkaf",
+            "p": "Sahure"
+        },
+        "answer": "m"
+    },
+    {
+        "question": "How many languages are estimated to be spoken around the world?",
+        "choices": {
+            "q": "8000",
+            "r": "2500",
+            "s": "500",
+            "t": "7100"
+        },
+        "answer": "t"
+    }
+]
+
+DISNEY_EASY = [
+    {
+        "question": "When did Disney World open to the public?",
+        "choices": {
+            "a": "December, 1965",
+            "b": "August, 1980",
+            "c": "October, 1971",
+            "d": "January, 1958"
+        },
+        "answer": "c"
+    },
+    {
+        "question": "Who are the Fab 5?",
+        "choices": {
+            "e": "Tinkerbell, Cinderella, Snow White, Peter Pan, and Fairy Godmother",
+            "f": "Mickey, Minnie, Donald, Goofy, and Pluto",
+            "g": "Mulan, Belle, Anna, Elsa, and Aurora",
+            "h": "Captain Jack Sparrow, Mirabel, Mrs. Incredible, Captain America, and Loki"
+        },
+        "answer": "f"
+    },
+    {
+        "question": "What is the smallest theme park at Disney World?",
+        "choices": {
+            "i": "EPCOT",
+            "j": "Disney's Animal Kingdom",
+            "k": "Magic Kingdom",
+            "l": "Hollywood Studios"
+        },
+        "answer": "k"
+    },
+    {
+        "question": "What is the fastest ride at Disney World?",
+        "choices": {
+            "m": "Test Track - 65 mph",
+            "n": "Rock ‘n’ Roller Coaster - 57mph",
+            "o": "Summit Plummet - 60mph",
+            "p": "The Monorail - 55 mph"
+        },
+        "answer": "m"
+    },
+    {
+        "question": "Which Disney park opened on Earth Day?",
+        "choices": {
+            "q": "Typhoon Lagoon",
+            "r": "Blizzard Beach",
+            "s": "Magic Kingdom",
+            "t": "Disney’s Animal Kingdom"
+        },
+        "answer": "t"
+    }
+]
+
+DISNEY_HARD = [
+    {
+        "question": "What is the biggest theme park at Disney World?",
+        "choices": {
+            "a": "Avengers Campus - 4 acres",
+            "b": "Epcot - 600 acres",
+            "c": "Disney’s Hollywood Studios - 154 acres",
+            "d": "Disney’s Animal Kingdom - 500 acres"
+        },
+        "answer": "d"
+    },
+    {
+        "question": "How tall is Cinderella’s castle?",
+        "choices": {
+            "e": "200ft",
+            "f": "160ft",
+            "g": "400ft",
+            "h": "189ft"
+        },
+        "answer": "h"
+    },
+    {
+        "question": "What are the three mountains called in Magic Kingdom?",
+        "choices": {
+            "i": "Everest Mountain, Thunder Mountain, Slinky Dog Dash Mountain",
+            "j": "Space Mountain, Splash Mountain and Big Thunder Mountain",
+            "k": "Expedition Everest, The Monorail Mountain, Splash Mountain",
+            "l": "The Twilight Zone Tower of Terror, Tower Speedway, The Barnstormer"
+        },
+        "answer": "j"
+    },
+    {
+        "question": "What is the maximum score you can get on Buzz Lightyear’s Space Ranger Spin?",
+        "choices": {
+            "m": "1,000,000",
+            "n": "600,000",
+            "o": "100,000",
+            "p": "999,999"
+        },
+        "answer": "p"
+    },
+    {
+        "question": "How fast is the drop in Splash Mountain?",
+        "choices": {
+            "q": "60 mph",
+            "r": "40 mph",
+            "s": "70 mph",
+            "t": "200 mph"
+        },
+        "answer": "r"
+    }
+]
+
+POP_CULTURE_EASY = [
+    {
+        "question": "How many kids do Kim Kardashian and Kanye West have together?",
+        "choices": {
+            "a": "3",
+            "b": "4",
+            "c": "5",
+            "d": "2"
+        },
+        "answer": "b"
+    },
+    {
+        "question": "How many Harry Potter books and movies are there?",
+        "choices": {
+            "e": "8 books and 9 movies",
+            "f": "9 books and 8 movies",
+            "g": "7 books and 8 movies",
+            "h": "8 books and 7 movies"
+        },
+        "answer": "g"
+    },
+    {
+        "question": "What is the name of Michelle Obama’s memoir written in 2018?",
+        "choices": {
+            "i": "Michelle Obama",
+            "j": "Becoming",
+            "k": "I am Michelle Obama",
+            "l": "American Grown"
+        },
+        "answer": "j"
+    },
+    {
+        "question": "What was the first non-English-language film to win Best Picture at the Oscars?",
+        "choices": {
+            "m": "Minari",
+            "n": "Amour",
+            "o": "Roma",
+            "p": "Parasite"
+        },
+        "answer": "p"
+    },
+    {
+        "question": "Who has the most Grammy awards?",
+        "choices": {
+            "q": "Georg Solti",
+            "r": "Stevie Wonder",
+            "s": "Quincy Jones",
+            "t": "Jay-Z"
+        },
+        "answer": "q"
+    }
+]
+
+POP_CULTURE_HARD = [
+    {
+        "question": "Who was named Forbes’ youngest self-made billionaire in 2019?",
+        "choices": {
+            "a": "Kylie Jenner",
+            "b": "Austin Russell",
+            "c": "Kevin David Lehmann",
+            "d": "Katharina Andresen"
+        },
+        "answer": "a"
+    },
+    {
+        "question": "How many Grammy Awards does Beyoncé have?",
+        "choices": {
+            "e": "29",
+            "f": "31",
+            "g": "28",
+            "h": "30"
+        },
+        "answer": "g"
+    },
+    {
+        "question": "Which is the highest grossing film?",
+        "choices": {
+            "i": "Avatar",
+            "j": "Avengers: Endgame",
+            "k": "Titanic",
+            "l": "Frozen II"
+        },
+        "answer": "i"
+    },
+    {
+        "question": "How many million people watched the most viewed Super Bowl broadcast?",
+        "choices": {
+            "m": "110",
+            "n": "112",
+            "o": "111",
+            "p": "114"
+        },
+        "answer": "p"
+    },
+    {
+        "question": "Who is the person with the highest Instagram follower count?",
+        "choices": {
+            "q": "Lionel Messi",
+            "r": "Gigi Hadid",
+            "s": "Cristiano Ronaldo",
+            "t": "Selena Gomez"
+        },
+        "answer": "s"
+    }
+]
