fd3d332d8e0e259c34ad2079ed8f646bac540960
PurpleFlower436 <bhargvih12@gmail.com>
2026-07-13 23:00:50 -0500

    added the calculate_points function so it checks the players answer against the correct answer and awards points accordingly.

--- utils.py
+++ utils.py
@@ -2,27 +2,22 @@ import random
 from questions import QUESTION_BANK
 
 
-#This function checks the lowercase version of the players answer against the lowercase version of the correct answer. 
-def check_answer(player_answer, correct_answer):
-    return player_answer.lower() == correct_answer.lower()
 
 
-#This function calculates points for a trivia question. If the question difficulty was easy then you get 5 points.
-# If the question difficulty was hard then you get 10 points. 
-def calculate_points(difficulty):
-    if difficulty == "easy":
-        return 5
+def calculate_points(player_answer, correct_answer ):
+
+    if player_answer.lower() == correct_answer:
+        return 10
     else:
-        return 10 
+        return 0
+    
 
-# This function determines the winner. If player 1's score was greater than player2's score then player 1 is the winner.
-# If player 2's score was greater than player 1's score then player 2 is the winner. 
-#If player 1 and player 2 scores equal each other then its a tie. 
+
 def determine_winner(player1, player2):
     if player1.score > player2.score:
-        return player1
+        return  player1.name + " " + "won!" + "🏆"
     elif player2.score > player1.score:
-        return player2
+        return player2.name + " " + "won!" + "🏆"
     
 
     else:
@@ -32,3 +27,17 @@ def determine_winner(player1, player2):
 
 def is_valid_input(user_input):
     return user_input.isalpha()
+
+
+
+def load_questions(category, difficulty):
+
+
+    questions = QUESTION_BANK[category][difficulty]
+
+    return questions
+
+
+
+
+    
