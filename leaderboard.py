ea91ce6462084645cfaf6f40b84d41124c699332
PurpleFlower436 <bhargvih12@gmail.com>
2026-07-15 13:35:17 -0500

    Created initialize_leaderboard function that has the player names and initial scores. Created update_leaderboard function which updates the players scores after they complete a trivia round.

--- leaderboard.py
+++ leaderboard.py
@@ -1,7 +1,7 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy import MetaData, Table, Column, Integer, String, Insert, create_engine, update
 
 
-
+engine = create_engine("sqlite:///player_scores.db")
 metadata_obj = MetaData()
 
 score_leaderboard = Table(
@@ -10,3 +10,24 @@ score_leaderboard = Table(
     Column("Name", String, primary_key=True),
     Column("Score", Integer, nullable= False)
 )
+
+metadata_obj.create_all(engine)
+
+
+def initialize_leaderboard(player_name, score):
+     with engine.begin() as conn:
+        initial_score = Insert(score_leaderboard).values(Name = player_name, Score = score)
+
+        conn.execute(initial_score)
+
+
+def update_leaderboard(player_name, score):
+
+   
+    with engine.begin() as conn:
+
+        stmt = (update(score_leaderboard)
+        .where(score_leaderboard.c.Name == player_name)
+        .values(Score=score))
+
+        conn.execute(stmt)
