10a68502f598807f933a7cddb051c2be2e20d611
PurpleFlower436 <bhargvih12@gmail.com>
2026-07-21 20:25:52 -0500

    Added input validation loop to ensure the user types in a valid answer so they can continue playing

--- trivia_v2.py
+++ trivia_v2.py
@@ -1,13 +1,16 @@
 from player import Player
+from rich import print
+import pandas as pd
 
 import random
-import sqlalchemy
-
+from sqlalchemy import select, desc
+from leaderboard import score_leaderboard, update_leaderboard, initialize_leaderboard, engine
 from utils import (
     
     calculate_points,
     determine_winner,
     load_questions,
+    is_valid_input
     
   
 )
@@ -16,28 +19,13 @@ from bots import choose_computer_bot_mode
 from questions import QUESTION_BANK
 
 
-"""
-
-ask the user for their name
-ask the user for the category and question difficulty
-
-create player objects 1 for the user and the other for the computer
- -> if the user types in a category that is not in the categories list then we make them try again and type in the category
-ask the user which bot they want to play against
-load the questions up and shuffle them 
-Iterate through the dictionary and have the user type in their answers
- then we call the calculate_points function which adds points to the players scores 
-then we add scores to the sql alchemy database
-
-once they reach 5 questions then you ask if you want to play again. if yes then we start again with a new category and difficulty level 
-if no then we quit and show the final leaderboard at the end. 
 
 
 
 
 
 
-"""
+
 
 
 def newGame():
@@ -53,7 +41,9 @@ def newGame():
 
     p2 = Player("Computer")
 
-
+    initialize_leaderboard(p1.name, p1.score)
+    initialize_leaderboard(p2.name, p2.score)
+    
     user_decision = "Y"
     while user_decision == "Y":
         user_picked_category = input(str("What category do you want? Type in the letter to the corresponding category \n a. marvel, b. history, c. pop culture, d. disney:"))
@@ -95,9 +85,13 @@ def newGame():
 
             computer_bot = choose_computer_bot_mode(computer_bot_choice, question)
 
+            while True:
+                p1_answer = input(str("Enter your answer player 1:"))
+                if is_valid_input(p1_answer):
+                    break
 
-            p1_answer = input(str("Enter your answer player 1:"))
-
+                else:
+                    print("Invalid answer choice! Please enter a, b , c, or d. ")
             bot_answer = computer_bot
 
             player_1_score = calculate_points(p1_answer, question["answer"])
@@ -106,10 +100,11 @@ def newGame():
 
             p1.add_points(player_1_score)
             print("")
-
-            print(p1.name, "current score:", p1.score)
+            update_leaderboard(p1.name, p1.score)
+            
             p2.add_points(player_2_score)
-            print(p2.name, "current score:",p2.score)
+            update_leaderboard(p2.name, p2.score)
+            
         
 
 
@@ -117,3 +112,18 @@ def newGame():
         user_decision = input(str("Do you want to keep playing? Type in Y for Yes or Q to quit:"))
         
 
+    winner = determine_winner(p1, p2)
+    print("\n", winner)
+
+    stmt = select(score_leaderboard)
+
+    with engine.connect() as connection:
+        df = pd.read_sql_query(stmt, connection)
+
+    
+    print("\n=======Current Leaderboard==========")
+    print(df.to_string(index=False))
+
+
+
+newGame()
